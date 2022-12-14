fa8d27f53bedae2f9ffe913c1094c904131b54ba
annakosheleva <71738312+annakosheleva@users.noreply.github.com>
2022-12-13 19:50:30 -0500

    fix linear search

--- 1_linear_search.js
+++ 1_linear_search.js
@@ -1,12 +1,15 @@
-const array = [1, 4, 5, 8, 5, 1, 2, 7, 5, 2, 11];
+let array = [1, 4, 5, 8, 5, 1, 2, 7, 5, 2, 11];
+let coint = 0;
 
 function linearSearch(array, item) {
 	for (let i = 0; i < array.length; i++) {
+		coint += 1;
 		if (array[i] === item) {
-			return i;
+			return i + 1;
 		}
 	}
 	return null;
 }
 
 console.log(linearSearch(array, 11));
+console.log("coint = ", coint);
